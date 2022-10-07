cafdd57e999a472a5b6279fa3e463c0f1206d9c3
Fernando Gomes <59961857+fernandogomesfg@users.noreply.github.com>
2022-10-07 16:10:57 +0200

    chore: actualizacao do mapa no ambiente

--- assets/js/meu-script.js
+++ assets/js/meu-script.js
@@ -2,11 +2,9 @@ let h2 = document.querySelector('h2')
 function success(pos) {
     console.log(pos.coords.latitude, pos.coords.longitude);
     h2.textContent = `Latitude: ${pos.coords.latitude}, Longitude: ${pos.coords.longitude}`;
-    var map = L.map('map').setView([pos.coords.latitude, pos.coords.longitude], 16);
+    var map = L.map('map').setView([pos.coords.latitude, pos.coords.longitude], 16.5);
 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
     L.marker([pos.coords.latitude, pos.coords.longitude]).addTo(map)
         .bindPopup('Voce esta aqui')
